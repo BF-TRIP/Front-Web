87d1d4479a632616b56e8f406795f481b2dff79f
daahyunk <ekgusdl0408@naver.com>
2025-03-05 16:11:00 +0900

    feat: 음성 인식 버튼 voice 메시지 보내는 기능 추가

--- src/pages/home/components/header-section/header-section.tsx
+++ src/pages/home/components/header-section/header-section.tsx
@@ -1,13 +1,30 @@
-import { BtnHomeVoice, IcCommonLogo, IcHomeBubble, IcHomeVoiceBg } from '@shared/assets/svg';
-import * as styles from './header-section.css';
+import {
+  BtnHomeVoice,
+  IcCommonLogo,
+  IcHomeBubble,
+  IcHomeVoiceBg,
+} from "@shared/assets/svg";
+import * as styles from "./header-section.css";
 
-const HeaderSection= () => {
+const HeaderSection = () => {
+  const handleVoiceSearch = () => {
+    if (window.webkit?.messageHandlers?.serverEvent) {
+      window.webkit.messageHandlers.serverEvent.postMessage("Voice");
+    } else {
+      console.error("iOS랑 연결 실패...");
+    }
+  };
+
   return (
     <header className={styles.header}>
-        <IcCommonLogo className={styles.logo}/>
+      <IcCommonLogo className={styles.logo} />
       <div className={styles.voiceSearchContainer}>
         <IcHomeVoiceBg className={styles.voiceBg} />
-        <BtnHomeVoice type='button' className={styles.voiceButton} />
+        <BtnHomeVoice
+          type="button"
+          className={styles.voiceButton}
+          onClick={handleVoiceSearch}
+        />
         <IcHomeBubble className={styles.voiceBubble} />
       </div>
     </header>
