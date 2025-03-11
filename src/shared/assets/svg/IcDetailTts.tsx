0278c6035f4ea2840687d7b8e419a0245ad57e9f
daahyunk <ekgusdl0408@naver.com>
2025-03-11 17:06:08 +0900

    feat: 장소 세부 페이지 아이콘 파일 추가

---
+++ src/shared/assets/svg/IcDetailTts.tsx
@@ -0,0 +1,3 @@
+import type { SVGProps } from "react";
+const SvgIcDetailTts = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 37 36" {...props}><rect width={36} height={36} x={0.679} fill="#1E1E1E" rx={18} /></svg>;
+export default SvgIcDetailTts;
