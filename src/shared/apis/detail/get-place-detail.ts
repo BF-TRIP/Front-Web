009ea65a51f64ad69bf1888cc67ceb7707d3dbfb
daahyunk <ekgusdl0408@naver.com>
2025-03-15 21:31:55 +0900

    refactor: instance 사용하게 수정

--- src/shared/apis/detail/get-place-detail.ts
+++ src/shared/apis/detail/get-place-detail.ts
@@ -1,9 +1,9 @@
+import { api } from "../config/instance";
 import { PlaceDetail } from "@pages/detail/types/detail-response";
 
 export const fetchPlaceDetail = async (contentId: number): Promise<PlaceDetail> => {
-  const response = await fetch(`http://211.254.215.190:8080/api/location/detail?contentid=${contentId}`);
-  if (!response.ok) {
-    throw new Error("Failed to fetch place detail");
-  }
-  return response.json();
+  const response = await api.get<PlaceDetail>(`/api/location/detail`, {
+    params: { contentid: contentId },
+  });
+  return response.data;
 };
