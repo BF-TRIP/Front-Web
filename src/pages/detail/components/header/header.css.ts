56d70a8ff7df37573b82482a3f3526d8f98d916f
daahyunk <ekgusdl0408@naver.com>
2025-05-09 09:07:22 +0900

    feat: pointer-events 명시적으로 설정

--- src/pages/detail/components/header/header.css.ts
+++ src/pages/detail/components/header/header.css.ts
@@ -1,36 +1,37 @@
 import { style } from "@vanilla-extract/css";
 
 export const header = style({
-    position: "absolute",
-    top: "2rem",
-    left: "2rem",
-    right: "2rem",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-  });
-  
-  export const backButton = style({
-    width: "3rem",
-    height: "3rem",
-    background: "none",
-    cursor: "pointer",
-  });
-  
-  export const scrapIcon = style({
-    width: "3rem",
-    height: "3rem",
-    cursor: "pointer",
-  });
-  
-  export const rightIcons = style({
-    display: "flex",
-    gap: "1rem",
-    alignItems: "center",
-  });
-  
-  export const ttsIcon = style({
-    width: "3rem",
-    height: "3rem",
-    cursor: "pointer",
-  });
+  position: "absolute",
+  top: "2rem",
+  left: "2rem",
+  right: "2rem",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+export const backButton = style({
+  width: "3rem",
+  height: "3rem",
+  background: "none",
+  cursor: "pointer",
+});
+
+export const scrapIcon = style({
+  width: "3rem",
+  height: "3rem",
+  cursor: "pointer",
+  pointerEvents: "auto",
+});
+
+export const rightIcons = style({
+  display: "flex",
+  gap: "1rem",
+  alignItems: "center",
+});
+
+export const ttsIcon = style({
+  width: "3rem",
+  height: "3rem",
+  cursor: "pointer",
+});
